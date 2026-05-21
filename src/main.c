ce3a780fd064888792b07c7520450e8fb5f1ecd2
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-05-22 00:43:42 +0530

    feat: Add basic usage

--- src/main.c
+++ src/main.c
@@ -1,11 +1,43 @@
 #include "common.h"
 #include "pos.h"
 
-int main() {
+// ========================================
+// helper declaration
+// ========================================
+
+static void usage(FILE *f);
+
+// ========================================
+// main
+// ========================================
+
+int main(int argc, const char **argv) {
+	if (argc == 1) {
+		usage(stderr);
+		return 1;
+	}
+
 	pos_t start = POS_INIT();
 
 	printf("Hello, World! start: %d | line: %d | column: %d\n", 
 		start.index, start.line, start.column);
 	return 0;
+}
+
+// ========================================
+// helper definition
+// ========================================
+
+static void usage(FILE *f) {
+	fprintf(f, 
+		"USAGE: pamelo [OPTIONS] <file>\n"
+		"\n"
+		"ABOUT:\n"
+		"    A Programming Language\n"
+		"\n"
+		"OPTIONS:\n"
+		"    --help, -h    This screen\n"
+		"\n"
+	);
 }
 
