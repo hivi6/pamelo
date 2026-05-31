60eab27ddbc98c8d768d3fbc856dddf7fbbe7e14
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-06-01 04:08:37 +0530

    refactor: Refactor ast.c

--- src/ast.c
+++ src/ast.c
@@ -6,19 +6,24 @@
 // helper declaration
 // ========================================
 
-static token_t *g_head;
-static token_t *g_cur;
+typedef struct parser_t parser_t;
+struct parser_t {
+	token_t *head;
+	token_t *cur;
+};
 
 static void print_ast_helper(ast_t *ast, char *indent, int depth, 
 	const char *extra);
 static char *token_str(token_t *token);
 
-static void init(token_t *tokens);
-static token_t *token_at(int offset);
-static char check(int offset, int token_kind);
-static token_t *match(int token_kind, const char *message);
-static void skip(int inc);
-static void append_token(token_t ***list, int *len, token_t *token);
+static void init(parser_t *parser, token_t *tokens);
+static token_t *token_at(parser_t *parser, int offset);
+static char check(parser_t *parser, int offset, int token_kind);
+static token_t *match(parser_t *parser, int token_kind, const char *message);
+static void skip(parser_t *parser, int inc);
+
+static void append_token(token_t ***list, int *len, 
+	token_t *token);
 
 static ast_t *malloc_ast(int kind, const char *filepath, const char *source,
 	pos_t start, pos_t end);
@@ -40,31 +45,32 @@ static ast_t *malloc_ast_cast_expr(ast_t *left, token_t *as_keyword,
 	ast_t *type_specifier);
 static ast_t *malloc_ast_add_expr(ast_t *left, token_t *op, ast_t *right);
 
-static ast_t *prog();
-static ast_t *decl();
-static ast_t *fn_decl();
-static ast_t *type_specifier();
-static ast_t *stmt();
-static ast_t *block_stmt();
-static ast_t *var_stmt();
-static ast_t *return_stmt();
-static ast_t *expr_stmt();
-static ast_t *expr();
-static ast_t *literal_expr();
-static ast_t *var_expr();
-static ast_t *primary_expr();
-static ast_t *postfix_expr();
-static ast_t *call_expr(ast_t *left);
-static ast_t *cast_expr();
-static ast_t *add_expr();
+static ast_t *prog(parser_t *parser);
+static ast_t *decl(parser_t *parser);
+static ast_t *fn_decl(parser_t *parser);
+static ast_t *type_specifier(parser_t *parser);
+static ast_t *stmt(parser_t *parser);
+static ast_t *block_stmt(parser_t *parser);
+static ast_t *var_stmt(parser_t *parser);
+static ast_t *return_stmt(parser_t *parser);
+static ast_t *expr_stmt(parser_t *parser);
+static ast_t *expr(parser_t *parser);
+static ast_t *literal_expr(parser_t *parser);
+static ast_t *var_expr(parser_t *parser);
+static ast_t *primary_expr(parser_t *parser);
+static ast_t *postfix_expr(parser_t *parser);
+static ast_t *call_expr(parser_t *parser, ast_t *left);
+static ast_t *cast_expr(parser_t *parser);
+static ast_t *add_expr(parser_t *parser);
 
 // ========================================
 // ast.h - definition
 // ========================================
 
 ast_t *parse(token_t *tokens) {
-	init(tokens);
-	return prog();
+	parser_t parser = {0};
+	init(&parser, tokens);
+	return prog(&parser);
 }
 
 void append_ast(ast_t ***list, int *len, ast_t *ast) {
@@ -116,7 +122,8 @@ static void print_ast_helper(ast_t *ast, char *indent, int depth,
 			depth+1, "RETURN TYPE");
 
 		for (int i = 0; i < ast->ast.fn_decl.params_len; i++) {
-			char *param = token_lexical(*ast->ast.fn_decl.params[i]);
+			token_t *param_token = ast->ast.fn_decl.params[i];
+			char *param = token_lexical(*param_token);
 			print_ast_helper(ast->ast.fn_decl.param_types[i], 
 				indent, depth+1, param);
 			free(param);
@@ -152,7 +159,8 @@ static void print_ast_helper(ast_t *ast, char *indent, int depth,
 		free(type_info);
 		free(name);
 		if (ast->ast.var_stmt.type_specifier) {
-			if (ast->ast.var_stmt.expr == NULL) indent[depth+1] = 0;
+			if (ast->ast.var_stmt.expr == NULL) 
+				indent[depth+1] = 0;
 			print_ast_helper(ast->ast.var_stmt.type_specifier, 
 				indent, depth+1, NULL);
 		}
@@ -256,12 +264,12 @@ static char *token_str(token_t *token) {
 	return res;
 }
 
-static void init(token_t *tokens) {
-	g_head = g_cur = tokens;
+static void init(parser_t *parser, token_t *tokens) {
+	parser->head = parser->cur = tokens;
 }
 
-static token_t *token_at(int offset) {
-	token_t *cur = g_cur;
+static token_t *token_at(parser_t *parser, int offset) {
+	token_t *cur = parser->cur;
 	while (offset && cur->kind != TOKEN_EOF) {
 		cur = cur->next;
 		offset--;
@@ -269,26 +277,26 @@ static token_t *token_at(int offset) {
 	return cur;
 }
 
-static char check(int offset, int token_kind) {
-	token_t *token = token_at(offset);
+static char check(parser_t *parser, int offset, int token_kind) {
+	token_t *token = token_at(parser, offset);
 	return token->kind == token_kind;
 }
 
-static token_t *match(int token_kind, const char *message) {
-	token_t *token = token_at(0);
+static token_t *match(parser_t *parser, int token_kind, const char *message) {
+	token_t *token = token_at(parser, 0);
 	if (token->kind != token_kind) {
 		eprintf(token->filepath, token->source, token->start, 
 			token->end, message);
 		exit(1);
 	}
-	skip(1);
+	skip(parser, 1);
 	return token;
 }
 
-static void skip(int inc) {
-	while (g_cur->kind != TOKEN_EOF && inc) {
+static void skip(parser_t *parser, int inc) {
+	while (parser->cur->kind != TOKEN_EOF && inc) {
 		inc--;
-		g_cur = g_cur->next;
+		parser->cur = parser->cur->next;
 	}
 }
 
@@ -414,10 +422,10 @@ static ast_t *malloc_ast_add_expr(ast_t *left, token_t *op, ast_t *right) {
 	return res;
 }
 
-static ast_t *prog() {
+static ast_t *prog(parser_t *parser) {
 	ast_t *ast = malloc_ast_prog();
-	while (!check(0, TOKEN_EOF)) {
-		ast_t *d = decl();
+	while (!check(parser, 0, TOKEN_EOF)) {
+		ast_t *d = decl(parser);
 		append_ast(&ast->ast.prog.decls, &ast->ast.prog.decls_len, d);
 		if (!ast->filepath) {
 			ast->filepath = d->filepath;
@@ -430,37 +438,39 @@ static ast_t *prog() {
 	return ast;
 }
 
-static ast_t *decl() {
-	return fn_decl();
+static ast_t *decl(parser_t *parser) {
+	return fn_decl(parser);
 }
 
-static ast_t *fn_decl() {
+static ast_t *fn_decl(parser_t *parser) {
 	token_t **params = NULL;
 	int params_len = 0;
 
 	ast_t **param_types = NULL;
 	int param_types_len = 0;
 
-	token_t *fn_keyword = match(TOKEN_FN_KEYWORD, "Expected fn keyword");
-	token_t *name = match(TOKEN_ID, "Expected name for function");
-	token_t *lparen = match(TOKEN_LPAREN, "Expected (");
+	token_t *fn_keyword = match(parser, TOKEN_FN_KEYWORD, 
+		"Expected fn keyword");
+	token_t *name = match(parser, TOKEN_ID, "Expected name for function");
+	token_t *lparen = match(parser, TOKEN_LPAREN, "Expected (");
 
-	while (!check(0, TOKEN_RPAREN)) {
-		token_t *id = match(TOKEN_ID, "Expected param name");
-		ast_t *type = type_specifier();
+	while (!check(parser, 0, TOKEN_RPAREN)) {
+		token_t *id = match(parser, TOKEN_ID, "Expected param name");
+		ast_t *type = type_specifier(parser);
 
 		append_token(&params, &params_len, id);
 		append_ast(&param_types, &param_types_len, type);
 
-		if (!check(0, TOKEN_COMMA)) break;
-		match(TOKEN_COMMA, "Expected ','");
+		if (!check(parser, 0, TOKEN_COMMA)) break;
+		match(parser, TOKEN_COMMA, "Expected ','");
 	}
 
-	token_t *rparen = match(TOKEN_RPAREN, "Expected )");
-	ast_t *t = type_specifier();
-	ast_t *s = block_stmt();
+	token_t *rparen = match(parser, TOKEN_RPAREN, "Expected )");
+	ast_t *t = type_specifier(parser);
+	ast_t *s = block_stmt(parser);
 
-	ast_t *res = malloc_ast_fn_decl(fn_keyword, name, lparen, rparen, t, s);
+	ast_t *res = malloc_ast_fn_decl(fn_keyword, name, lparen, rparen, t, 
+		s);
 	res->ast.fn_decl.params = params;
 	res->ast.fn_decl.params_len = params_len;
 	res->ast.fn_decl.param_types = param_types;
@@ -468,27 +478,27 @@ static ast_t *fn_decl() {
 	return res;
 }
 
-static ast_t *type_specifier() {
-	token_t *id = match(TOKEN_ID, "Expected type id");
+static ast_t *type_specifier(parser_t *parser) {
+	token_t *id = match(parser, TOKEN_ID, "Expected type id");
 	return malloc_ast_type_specifier(id);
 }
 
-static ast_t *stmt() {
-	if (check(0, TOKEN_LBRACE)) return block_stmt();
-	if (check(0, TOKEN_VAR_KEYWORD)) return var_stmt();
-	if (check(0, TOKEN_RETURN_KEYWORD)) return return_stmt();
-	return expr_stmt();
+static ast_t *stmt(parser_t *parser) {
+	if (check(parser, 0, TOKEN_LBRACE)) return block_stmt(parser);
+	if (check(parser, 0, TOKEN_VAR_KEYWORD)) return var_stmt(parser);
+	if (check(parser, 0, TOKEN_RETURN_KEYWORD)) return return_stmt(parser);
+	return expr_stmt(parser);
 }
 
-static ast_t *block_stmt() {
-	token_t *lbrace = match(TOKEN_LBRACE, "Expected {");
+static ast_t *block_stmt(parser_t *parser) {
+	token_t *lbrace = match(parser, TOKEN_LBRACE, "Expected {");
 	ast_t *ast = malloc_ast_block_stmt(lbrace);
-	while (!check(0, TOKEN_RBRACE)) {
-		ast_t *s = stmt();
+	while (!check(parser, 0, TOKEN_RBRACE)) {
+		ast_t *s = stmt(parser);
 		append_ast(&ast->ast.block_stmt.stmts, 
 			&ast->ast.block_stmt.stmts_len, s);
 	}
-	token_t *rbrace = match(TOKEN_RBRACE, "Expected }");
+	token_t *rbrace = match(parser, TOKEN_RBRACE, "Expected }");
 
 	ast->ast.block_stmt.rbrace = rbrace;
 	ast->end = rbrace->end;
@@ -496,95 +506,98 @@ static ast_t *block_stmt() {
 	return ast;
 }
 
-static ast_t *var_stmt() {
-	token_t *var_keyword = match(TOKEN_VAR_KEYWORD, 
+static ast_t *var_stmt(parser_t *parser) {
+	token_t *var_keyword = match(parser, TOKEN_VAR_KEYWORD, 
 		"Expected 'var' keyword");
-	token_t *name = match(TOKEN_ID, "Expected var name");
+	token_t *name = match(parser, TOKEN_ID, "Expected var name");
 	ast_t *t = NULL;
 	ast_t *e = NULL;
 
-	if (!check(0, TOKEN_SEMICOLON) && !check(0, TOKEN_EQUAL)) {
-		t = type_specifier();
+	if (!check(parser, 0, TOKEN_SEMICOLON) && 
+		!check(parser, 0, TOKEN_EQUAL)) {
+		t = type_specifier(parser);
 	}
 
-	if (check(0, TOKEN_EQUAL)) {
-		skip(1);
-		e = expr();
+	if (check(parser, 0, TOKEN_EQUAL)) {
+		skip(parser, 1);
+		e = expr(parser);
 	}
 
-	token_t *semicolon = match(TOKEN_SEMICOLON, 
+	token_t *semicolon = match(parser, TOKEN_SEMICOLON, 
 		"Expected ';' at the end of var statement");
 
 	return malloc_ast_var_stmt(var_keyword, name, t, e, semicolon);
 }	
 
-static ast_t *return_stmt() {
-	token_t *return_keyword = match(TOKEN_RETURN_KEYWORD,
+static ast_t *return_stmt(parser_t *parser) {
+	token_t *return_keyword = match(parser, TOKEN_RETURN_KEYWORD,
 		"Expected 'return' keyword");
 	ast_t *e = NULL;
-	if (!check(0, TOKEN_SEMICOLON)) {
-		e = expr();
+	if (!check(parser, 0, TOKEN_SEMICOLON)) {
+		e = expr(parser);
 	}
-	token_t *semicolon = match(TOKEN_SEMICOLON, 
+	token_t *semicolon = match(parser, TOKEN_SEMICOLON, 
 		"Expected ';' at the end of return statement");
 	return malloc_ast_return_stmt(return_keyword, e, semicolon);
 }
 
-static ast_t *expr_stmt() {
-	ast_t *e = expr();
-	token_t *semicolon = match(TOKEN_SEMICOLON, "Expected ;");
+static ast_t *expr_stmt(parser_t *parser) {
+	ast_t *e = expr(parser);
+	token_t *semicolon = match(parser, TOKEN_SEMICOLON, "Expected ;");
 	return malloc_ast_expr_stmt(e, semicolon);
 }
 
-static ast_t *expr() {
-	return add_expr();
+static ast_t *expr(parser_t *parser) {
+	return add_expr(parser);
 }
 
-static ast_t *literal_expr() {
-	token_t *token = match(TOKEN_INT_LITERAL, "Expected int literal");
+static ast_t *literal_expr(parser_t *parser) {
+	token_t *token = match(parser, TOKEN_INT_LITERAL, 
+		"Expected int literal");
 	return malloc_ast_literal_expr(token);
 }
 
-static ast_t *var_expr() {
-	token_t *token = match(TOKEN_ID, "Expected var literal");
+static ast_t *var_expr(parser_t *parser) {
+	token_t *token = match(parser, TOKEN_ID, "Expected var literal");
 	return malloc_ast_var_expr(token);
 }
 
-static ast_t *primary_expr() {
-	if (check(0, TOKEN_INT_LITERAL)) return literal_expr();
-	if (check(0, TOKEN_ID)) return var_expr();
+static ast_t *primary_expr(parser_t *parser) {
+	if (check(parser, 0, TOKEN_INT_LITERAL)) return literal_expr(parser);
+	if (check(parser, 0, TOKEN_ID)) return var_expr(parser);
 	
-	token_t *tok = token_at(0);
+	token_t *tok = token_at(parser, 0);
 	eprintf(tok->filepath, tok->source, tok->start, tok->end,
 		"What is this primary expr?");
 	exit(1);
 }
 
-static ast_t *postfix_expr() {
-	ast_t *left = primary_expr();
-	while (check(0, TOKEN_LPAREN)) {
-		if (check(0, TOKEN_LPAREN)) left = call_expr(left);
+static ast_t *postfix_expr(parser_t *parser) {
+	ast_t *left = primary_expr(parser);
+	while (check(parser, 0, TOKEN_LPAREN)) {
+		if (check(parser, 0, TOKEN_LPAREN)) 
+			left = call_expr(parser, left);
 	}
 
 	return left;
 }
 
-static ast_t *call_expr(ast_t *left) {
+static ast_t *call_expr(parser_t *parser, ast_t *left) {
 	ast_t **args = NULL;
 	int args_len = 0;
 
-	token_t *lparen = match(TOKEN_LPAREN, "Expected '('");
+	token_t *lparen = match(parser, TOKEN_LPAREN, "Expected '('");
 	
-	while (!check(0, TOKEN_RPAREN)) {
-		ast_t *arg = expr();
+	while (!check(parser, 0, TOKEN_RPAREN)) {
+		ast_t *arg = expr(parser);
 
 		append_ast(&args, &args_len, arg);
 		
-		if (!check(0, TOKEN_COMMA)) break;
-		match(TOKEN_COMMA, "Expected ','");
+		if (!check(parser, 0, TOKEN_COMMA)) break;
+		match(parser, TOKEN_COMMA, "Expected ','");
 	}
 
-	token_t *rparen = match(TOKEN_RPAREN, "Expected ')'");
+	token_t *rparen = match(parser, TOKEN_RPAREN, "Expected ')'");
 
 	ast_t *res = malloc_ast_call_expr(left, lparen, rparen);
 	res->ast.call_expr.args = args;
@@ -592,23 +605,23 @@ static ast_t *call_expr(ast_t *left) {
 	return res;
 }
 
-static ast_t *cast_expr() {
-	ast_t *left = postfix_expr();
-	if (check(0, TOKEN_AS_KEYWORD)) {
-		token_t *as_keyword = token_at(0);
-		skip(1);
-		ast_t *t = type_specifier();
+static ast_t *cast_expr(parser_t *parser) {
+	ast_t *left = postfix_expr(parser);
+	if (check(parser, 0, TOKEN_AS_KEYWORD)) {
+		token_t *as_keyword = token_at(parser, 0);
+		skip(parser, 1);
+		ast_t *t = type_specifier(parser);
 		left = malloc_ast_cast_expr(left, as_keyword, t);
 	}
 	return left;
 }
 
-static ast_t *add_expr() {
-	ast_t *left = cast_expr();
-	while (check(0, TOKEN_PLUS) || check(0, TOKEN_MINUS)) {
-		token_t *op = token_at(0);
-		skip(1);
-		ast_t *right = cast_expr();
+static ast_t *add_expr(parser_t *parser) {
+	ast_t *left = cast_expr(parser);
+	while (check(parser, 0, TOKEN_PLUS) || check(parser, 0, TOKEN_MINUS)) {
+		token_t *op = token_at(parser, 0);
+		skip(parser, 1);
+		ast_t *right = cast_expr(parser);
 		left = malloc_ast_add_expr(left, op, right);
 	}
 	return left;
